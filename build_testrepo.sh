3dec49f5503826b5fe584426da130361630052b9
daniel-e <git.daniele@gmail.com>
2018-12-18 01:39:00 +0100

    update

--- build_testrepo.sh
+++ build_testrepo.sh
@@ -4,10 +4,7 @@
 rm -rf repo
 mkdir repo
 cd repo
-../paper.py init
-cp ../testdata/*.pdf .
-for i in *.pdf; do 
-	echo $i
-	../paper.py add $i
+../papr/cli.py init
+for i in 1812.06081 1812.06080 1812.06071 1812.06061 1812.06060 1812.06055 1812.06051 1812.06050 1812.06038; do
+	../papr/cli.py fetch https://arxiv.org/abs/$i
 done
-
