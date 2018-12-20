1256436098955739b7198b54b15afbb528e280b2
daniel-e <git.daniele@gmail.com>
2018-12-20 11:07:58 +0100

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="papr",
-    version="0.0.9",
+    version="0.0.10",
     entry_points = {
         "console_scripts": ['papr = papr.cli:main']
         },
