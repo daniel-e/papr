0dab5faf314fdfa85c75ff5edc6900f007737cc1
daniel-e <git.daniele@gmail.com>
2019-05-17 21:42:10 +0200

    scrollview

--- papr/lib/ui.py
+++ papr/lib/ui.py
@@ -9,6 +9,11 @@ from .tools import filter_list, show_pdf
 from .cmd_fetch import NEWTAG
 
 
+def write(s):
+    sys.stdout.write(s)
+    sys.stdout.flush()
+
+
 def expand_to_colwidth(s: str):
     c = cols()
     return s + (" " * (c - len(s)))
@@ -19,7 +24,7 @@ def hr():
 
 
 def build_header():
-    return "Id    Title " + " " * (cols() - 12 - 8) + "Stars" + "  " + "\n" + hr()
+    return "Id    Title " + " " * (cols() - 12 - 8) + "Stars" + "   " + hr()
 
 
 def build_default_header():
@@ -31,7 +36,41 @@ def build_default_header():
 def build_search_header():
     return termcolor.colored(
         expand_to_colwidth("ESC: cancel + back to selection mode | ENTER: back to selection mode"),
+        "white", "on_yellow", attrs=["bold"])
+
+
+def build_title():
+    return termcolor.colored(
+        expand_to_colwidth("papr"),
         "white", "on_blue", attrs=["bold"])
+
+
+# The last element is selected.
+class ScrollView():
+    def __init__(self, n_elements, rows, selected):
+        self.n_elemens = n_elements
+        self.rows = rows
+        self._selected = selected    # selected is in [0..n_elements-1]
+        self.first_element_in_view = max(0, n_elements - rows)
+
+    def up(self):
+        if self.first_element_in_view == self._selected:
+            self.first_element_in_view = max(self.first_element_in_view - 1, 0)
+        self._selected = max(0, self._selected - 1)
+
+    def down(self):
+        if self._selected == self.first_element_in_view + self.rows - 1:
+            self.first_element_in_view = min(self.first_element_in_view + 1, self.n_elemens - self.rows)
+        self._selected = min(self._selected + 1, self.n_elemens - 1)
+
+    def selected(self):
+        return self._selected
+
+    def first(self):
+        return self.first_element_in_view
+
+    def end(self):
+        return self.first_element_in_view + self.rows
 
 
 def run_ui(args, repo):
@@ -43,32 +82,36 @@ def run_ui(args, repo):
     if len(args) > 0:
         r = filter_list(r, args[0])
 
-    print(build_header())
-    m = rows() - 4 - 1             # have added -1 as otherwise the header flickers sometimes when scrolling
-    n = len(r)
-    window_rows = min(m, n)        # number of rows of the view
-    view = n - window_rows         # index in r of the first element in the view
-    selected = window_rows - 1     # row selected within the view
-    initial_window_rows = window_rows
+    cursor_off()
+    write(build_title())
+
+    n_window_rows = rows() - 1
+
+    n_papers = len(r)
+    n_view_rows = n_window_rows - 3 - 1
+    # -3 = 3 rows for header
+    # -1 = 1 row for search line
+    v = ScrollView(n_elements=n_papers, rows=n_view_rows, selected=n_papers-1)
+
     search = ""
     in_search = False
-    cursor_off()
     papers = r[:]
-    while True:
-        cursor_up(4)
-        if in_search:
-            print(build_search_header())
-        else:
-            print(build_default_header())
-        print(build_header())
 
-        cnt = 0
-        for idx, p in enumerate(papers[view:view + m]):
-            print_paper(p, idx == selected)
+    while True:
+        if in_search:
+            write(build_search_header())
+        else:
+            write(build_default_header())
+        write(build_header())
+
+        cnt = 0 + 3
+        for idx, p in enumerate(papers[v.first():v.end()]):
+            print_paper(p, idx + v.first() == v.selected())
             cnt += 1
-        while cnt < initial_window_rows:
+        # clear remaining lines
+        while cnt < n_window_rows - 1:
             cnt += 1
-            print(empty_line())
+            write(empty_line())
 
         # search line
         sys.stdout.write(empty_line() + "\r")
@@ -76,86 +119,63 @@ def run_ui(args, repo):
             sys.stdout.write("search: " + search + "▃")
             sys.stdout.flush()
 
+        cnt += 1
+
         k = read_key()
         if k is None:
-            cursor_up(initial_window_rows + 1)
+            cursor_up(cnt)
             continue
         if not in_search:
             if k == 'l':
                 list_of_tags(repo)
-                cursor_up(initial_window_rows + 1)
             elif k == 'a':
-                abstract_of_paper(papers[view + selected])
-                cursor_up(initial_window_rows + 1)
+                abstract_of_paper(papers[v.selected()])
             elif k == 'y':
-                details_of_paper(papers[view + selected])
-                cursor_up(initial_window_rows + 1)
+                details_of_paper(papers[v.selected()])
             elif k == 'i' or k == '#':
-                if selected == 0:
-                    view = max(view - 1, 0)
-                else:
-                    selected -= 1
-                cursor_up(initial_window_rows + 1)
+                v.up()
             elif k == 'k' or k == '+':
-                if selected == window_rows - 1:
-                    if selected + view + 1 < n:
-                        view += 1
-                    pass
-                else:
-                    selected += 1
-                cursor_up(initial_window_rows + 1)
+                v.down()
             elif k == 'n':
-                notes_of_paper(repo, papers[view + selected])
-                cursor_up(initial_window_rows + 1)
+                notes_of_paper(repo, papers[v.selected()])
             elif k == 't':
-                tags_of_paper(repo, papers[view + selected])
-                cursor_up(initial_window_rows + 1)
+                tags_of_paper(repo, papers[v.selected()])
             elif ord(k) == 10:
-                papers[view + selected].remove_tag(NEWTAG)
-                repo.update_paper(papers[view + selected])
-                show_pdf(papers[view + selected], repo.pdf_path())
-                cursor_up(initial_window_rows + 1)
+                p = v.selected()
+                papers[p].remove_tag(NEWTAG)
+                repo.update_paper(papers[p])
+                show_pdf(papers[p], repo.pdf_path())
             elif ord(k) == 27 or k == 'q':
                 break
             elif k == 's':
-                cursor_up(initial_window_rows + 1)
                 in_search = True
-            elif k >= '0' and k <= '5':
+            elif '0' <= k <= '5':
                 stars = int(k)
-                papers[view + selected].set_stars(stars)
-                repo.update_paper(papers[view + selected])
-                cursor_up(initial_window_rows + 1)
-            else:
-                cursor_up(initial_window_rows + 1)
+                papers[v.selected()].set_stars(stars)
+                repo.update_paper(papers[v.selected()])
         else:
             update_search = False
             if ord(k) == 27:
                 in_search = False
                 search = ""
-                cursor_up(initial_window_rows + 1)
                 update_search = True
             elif ord(k) == 10:
-                in_search= False
-                cursor_up(initial_window_rows + 1)
+                in_search = False
             elif ord(k) == 127:
                 search = search[:-1]
-                cursor_up(initial_window_rows + 1)
                 update_search = True
             else:
                 search += k
-                cursor_up(initial_window_rows + 1)
                 update_search = True
+
             if update_search:
                 if len(search) == 0:
                     papers = r[:]
                 else:
                     papers = filter_list(r, search)
-                m = rows() - 4 - 1
-                n = len(papers)
-                window_rows = min(m, n)        # number of rows of the view
-                view = n - window_rows         # index in r of the first element in the view
-                selected = window_rows - 1     # row selected within the view
 
+            v = ScrollView(n_elements=len(papers), rows=n_view_rows, selected=len(papers)-1)
+        cursor_up(cnt)
     cursor_on()
     print()
 
